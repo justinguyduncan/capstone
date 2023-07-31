e923420c9b809b9cdf52114f1f8555f457932c2b
Justin Duncan <justinguyduncan@hotmail.com>
2023-07-31 08:23:15 -0700

    aws

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -6,14 +6,20 @@ from sqlalchemy.sql import text
 def seed_users():
     demo = User(
         username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+    justin = User(
+        username = 'TruePr0tege', email='justin@aa.io', password = 'password')
+    rajheem = User(
+        username = 'DV Bigred1', email = 'rajheem@aa.io', password = 'password')
+    chris = User(
+        username = 'DUNC', email = 'chris@aa.io', password = 'password')
+    john = User(
+        username = 'Reaper4G', email = 'john@aa.io', password = 'password')
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+     db.session.add(justin)
+    db.session.add(rajheem)
+    db.session.add(chris)
+    db.session.add(john)
     db.session.commit()
 
 
@@ -28,5 +34,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
