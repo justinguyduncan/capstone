2cfbac3b0acb9660fa761eddf911be1695ea5823
Justin Duncan <justinguyduncan@hotmail.com>
2023-08-07 21:51:14 -0700

    changed order of seeds undo command

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -22,11 +22,11 @@ def seed():
         # command, which will truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_channels()
-        undo_vods()
-        undo_subscribers()
         undo_comments()
+        undo_subscribers()
+        undo_vods()
+        undo_channels()
+        undo_users()
     seed_users()
     seed_channels()
     seed_vods()
@@ -38,9 +38,9 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
-    undo_channels()
-    undo_vods()
-    undo_subscribers()
-    undo_comments()
+        undo_comments()
+        undo_subscribers()
+        undo_vods()
+        undo_channels()
+        undo_users()
     # Add other undo functions here
